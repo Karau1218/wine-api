466bcf5fdea8fe3a109659af05455a7afc4bbd18
Fkarau <30944677+Karau1218@users.noreply.github.com>
2026-01-29 11:39:44 -0800

    in verb1

---
+++ src/controllers/wine.controller.js
@@ -0,0 +1,4 @@
+//place controller functions here...
+import * as wineService from "../services/wine.service.js";
+
+// export const getAllWines = ( req, res) =
--- src/routers/default.routes.js
+++ src/routers/default.routes.js
@@ -1,6 +1,22 @@
+// FINISH VERB1 - ROUTER
 import { Router } from "express";
+import * as wineController from "../controllers/wine.controller.js"
 
 const router = Router();
+// Get all wines
+export const getAllWines = (req, res) => {
+    let result = tickets;
+
+}
+// router.get("/wines", wineController.getAllWines);
+
+// Get wines by id
+router.get("/wines/:id", wineController.getWinebyId);
+
+export const wineById = (req, res) => {
+    const { id }
+}
+
 
 router.get("/", (req, res) => {
     res.render("default", {
@@ -8,5 +24,6 @@ router.get("/", (req, res) => {
         subtitle: "Express + EJS + Static Assets"
     });
 });
+
 
 export default router;
